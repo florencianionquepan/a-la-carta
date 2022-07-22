52f12c24cd01a2c7fd2113fd494f4e08cc24446c
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-07-22 16:37:05 -0300

    crea el guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { GuardGuard } from './services/guard.guard';
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent},
+  {path:'home',component:HomeComponent, canActivate:[GuardGuard]},
   {path:'login',component:LoginComponent},
   {path:'',redirectTo:'login',pathMatch: "full"}
 ];

d87b585c8b2e5fb49611376ec7b7af01f41cf321
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-07-26 16:36:02 -0300

    Modifica add component

--- src/app/components/add/add.component.ts
+++ src/app/components/add/add.component.ts
@@ -12,21 +12,27 @@ import Swal from 'sweetalert2';
 })
 export class AddComponent implements OnInit {
   @Input() idPlato: number = 1;
-  public platoDetalles: Dish={id:1,title:'',image:'',description:'',price:0,preparationMinutes:0,healthScore:0,vegan:true}
-  platosMenu:Dish[]=[];
-  private agregado:boolean=false;
-  private platosVeganos:number=0;
-  public mensaje:string="No pasé por ningún lado...";
+  public platoDetalles: Dish;
+  
+  public platosMenu:Dish[];
+  public agregado:boolean;
+  public platosVeganos:number;
+  public mensaje:string;
 
-  constructor(private searcherSvc: SearcherService, private menuSvc: MenuService) { }
-
-  ngOnInit(): void {
+  constructor(private searcherSvc: SearcherService, private menuSvc: MenuService) { 
+    this.platoDetalles={id:1,title:'',image:'',description:'',price:0,preparationMinutes:0,healthScore:0,vegan:true};
+    this.platosMenu=[];
+    this.agregado=true;
+    this.platosVeganos=0;
+    this.mensaje='This dish was added to the menu';
   }
+
+  ngOnInit(): void { }
 
   public obtenerPlato(id: number) {
     this.searcherSvc.verDetalles(id).subscribe({
       next: (data: any) => {
-        console.log(data.id);
+        //console.log(data.id);
         this.platoDetalles.id = data.id;
         this.platoDetalles.title = data.title;
         this.platoDetalles.image = data.image;
@@ -35,8 +41,8 @@ export class AddComponent implements OnInit {
         this.platoDetalles.preparationMinutes = data.preparationMinutes;
         this.platoDetalles.healthScore = data.healthScore;
         this.platoDetalles.vegan = data.vegan;
-        console.log(this.platoDetalles);
-        
+        //una vez que obtengo plato detalles. llama a seleccionarPlato;
+        this.seleccionarPlato(id);
       },
       error:(error:HttpErrorResponse)=>{
         console.log(error.message);
@@ -44,9 +50,24 @@ export class AddComponent implements OnInit {
     })
   }
 
-  public controlarMenu(plato:Dish){
-    this.platosVeganos=0;
+  public seleccionarPlato(id:number){ 
+    //obtengo todo el listado de platos del menu para controlar que el plato seleccionado pueda ser añadido
     this.platosMenu=this.menuSvc.getPlatos();
+    console.log(this.platosMenu);
+    if(this.platosMenu.length<2){
+      this.anadirPlato(this.platoDetalles);
+    }else if(this.platosMenu.length==4){
+      this.agregado=false;
+      this.mensaje="It is not possible add it to the menu. You already order 4 dishes";
+    }else if(this.platosMenu.length>=2 && this.platosMenu.length<4){
+      console.log('tengo entre dos y 4 platos');
+      this.controlarMenu(id);
+    }
+    Swal.fire(this.mensaje);
+  }
+
+  public controlarMenu(id:number){
+    this.platosVeganos=0;
     if(this.platosMenu.length==2){
       //Contamos platos veganos hsta el momento:
       this.platosMenu.forEach((plato)=>{
@@ -54,23 +75,24 @@ export class AddComponent implements OnInit {
           this.platosVeganos++;
         }
       })
+      console.log(this.platosVeganos);
       if(this.platosVeganos==2){
         //EL PLATO A AÑADIR DEBE SER NO VEGANO
-        if(plato.vegan){
+        if(this.platoDetalles.vegan){
           this.agregado=false;
           this.mensaje="The next dish to add to the menu should not be vegan";
         }else{
-          this.añadirPlato(this.platoDetalles);
+          this.anadirPlato(this.platoDetalles);
         }
       }else if(this.platosVeganos==1){
-        this.añadirPlato(this.platoDetalles);
+        this.anadirPlato(this.platoDetalles);
       }else if(this.platosVeganos==0){
         //EL PLATO A AÑADIR DEBE SER VEGANO
-        if(!plato.vegan){
+        if(!this.platoDetalles.vegan){
           this.agregado=false;
           this.mensaje="The next dish to add to the menu should be vegan";
         }else{
-          this.añadirPlato(this.platoDetalles);
+          this.anadirPlato(this.platoDetalles);
         }
       }
     }else if(this.platosMenu.length==3){
@@ -81,43 +103,25 @@ export class AddComponent implements OnInit {
       })
       if(this.platosVeganos==2){
         //EL PLATO A AÑADIR DEBE SER NO VEGANO
-        if(plato.vegan){
+        if(this.platoDetalles.vegan){
           this.agregado=false;
           this.mensaje="The next dish to add to the menu should not be vegan";
         }
       }else if(this.platosVeganos==1){
         //EL PLATO A AÑADIR DEBE SER VEGANO
-        if(!plato.vegan){
+        if(!this.platoDetalles.vegan){
           this.agregado=false;
           this.mensaje="The next dish to add to the menu should be vegan";
         }else{
-          this.añadirPlato(this.platoDetalles);
+          this.anadirPlato(this.platoDetalles);
         }
       }
     }
   }
 
-  public seleccionarPlato(id:number){
-    //por defecto this.agregado sera true, y el mensaje ok:
-    this.agregado=true;
-    this.mensaje="This dish was added to the menu";
-    //aca voy a crear el plato con el id recibido & this.platoDetalles tendra todos sus atributos
-    this.obtenerPlato(id);
-    //obtengo todo el listado de platos del menu para controlar que el plato seleccionado pueda ser añadido
-    this.platosMenu=this.menuSvc.getPlatos();
-    if(this.platosMenu.length<2){
-      this.añadirPlato(this.platoDetalles);
-    }else if(this.platosMenu.length==4){
-      this.agregado=false;
-      this.mensaje="It is not possible add it to the menu. You already order 4 dishes";
-    }else if(this.platosMenu.length>=2 && this.platosMenu.length<4){
-      this.controlarMenu(this.platoDetalles);
-    }
-    Swal.fire(this.mensaje);
+  public anadirPlato(plato:Dish){
+    this.menuSvc.agregarPlato(plato);
   }
 
-  public añadirPlato(platoNuevo:Dish){
-    this.menuSvc.agregarPlato(platoNuevo);
-  }
 
 }

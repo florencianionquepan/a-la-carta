e792442d1c0b050bd689abac0fd38971f4db58e8
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-07-28 17:12:22 -0300

    Se agrega al home los acumulativos y promedios solicitados

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -9,17 +9,51 @@ import { MenuService } from 'src/app/services/menu.service';
 })
 export class MenuComponent implements OnInit {
   public platosPedidos:Dish[];
+  public totalPrice:string='0';
+  public timePreparation:string='0';
+  public healthScore:string='0';
 
   constructor(private menuSvc:MenuService) { 
     this.platosPedidos=this.menuSvc.getPlatos();
   }
 
   ngOnInit(): void {
+    this.hacerCalculos();
     this.menuSvc.actualizarPedidoObservable.subscribe(platos=>{
       this.platosPedidos=platos;
-/*       console.log('ngOnInitMenuComponent');
-      console.log(this.platosPedidos); */
+      this.hacerCalculos();
     })
+  }
+
+  private hacerCalculos():void{
+    this.totalPrice=this.calcularPrecio().toFixed(2);
+    this.timePreparation=this.calcularTiempo().toFixed(2);
+    this.healthScore=this.calcularHS().toFixed(2);
+  }
+
+
+  private calcularPrecio():number{
+    let suma=0;
+    this.platosPedidos.forEach((plato)=>{
+      suma+=plato.price;
+    })
+    return suma;
+  }
+
+  private calcularTiempo():number{
+    let tiempoTot=0;
+    this.platosPedidos.forEach((plato)=>{
+      tiempoTot+=plato.preparationMinutes;
+    })
+    return tiempoTot/this.platosPedidos.length;
+  }
+
+  private calcularHS():number{
+    let healthS=0;
+    this.platosPedidos.forEach((plato)=>{
+      healthS+=plato.healthScore;
+    })
+    return healthS/this.platosPedidos.length;
   }
 
 }

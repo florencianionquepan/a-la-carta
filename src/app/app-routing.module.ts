21e3451ee97338dbc0bd4c36c6d969a2a41a9a99
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-07-29 14:22:49 -0300

    Se agrega nueva ruta de detalles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { GuardGuard } from './services/guard.guard';
+import { DishDetailComponent } from './components/dish-detail/dish-detail.component';
 
 const routes: Routes = [
   {path:'home',component:HomeComponent, canActivate:[GuardGuard]},
   {path:'login',component:LoginComponent},
+  {path:'detail/:id', component:DishDetailComponent, canActivate:[GuardGuard]},
   {path:'',redirectTo:'login',pathMatch: "full"}
 ];
 

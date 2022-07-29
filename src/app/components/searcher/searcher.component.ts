8a2f6b41e3986a747216cba400fd40ec6f17ccd5
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-07-29 15:52:53 -0300

    Se ocultan los resultados cuando se borra el texto en el buscador

--- src/app/components/searcher/searcher.component.ts
+++ src/app/components/searcher/searcher.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { debounce, filter, interval, map, Subject } from 'rxjs';
+import { debounce, distinctUntilChanged, filter, interval, map, Subject } from 'rxjs';
 import { SearcherService } from 'src/app/services/searcher.service';
 
 
@@ -10,16 +10,26 @@ import { SearcherService } from 'src/app/services/searcher.service';
 })
 export class SearcherComponent implements OnInit {
   public resultadoLista: Array<any>=[]
+  public busqueda:boolean=false;
 
   public search=new Subject<any>();
   constructor(private searcherService:SearcherService) {
-    const busqueda=this.search.pipe(
+    this.search.pipe(
       map((event:any)=>event.target.value),
       filter(text => text.length > 2),
       debounce(()=>interval(500)),
     ).subscribe({
       next:(text)=>{
         this.obtener(text);
+      }
+    })
+    this.search.pipe(
+      map((event:any)=>event.target.value),
+      filter(text=>text.length==0)
+    ).subscribe({
+      next:()=>{
+        this.resultadoLista=[];
+        this.busqueda=false;
       }
     })
   }
@@ -31,7 +41,7 @@ export class SearcherComponent implements OnInit {
     this.searcherService.verPlatos(text).subscribe({
       next:(data:any)=>{
         this.resultadoLista=data;
-        //console.log(this.resultadoLista);
+        this.busqueda=true;
       }
     })
   }
